5ff45cb4951885feb092780399c3a1d206a38318
isjmovo <isjmovo@gmail.com>
2024-01-09 12:57:31 +0900

    answer 조회

--- src/test/java/com/ll/exam/sbb/AnswerRepositoryTests.java
+++ src/test/java/com/ll/exam/sbb/AnswerRepositoryTests.java
@@ -7,6 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 class AnswerRepositoryTests {
 	@Autowired
@@ -23,6 +25,20 @@ class AnswerRepositoryTests {
 
 	private void createSampleData() {
 		QuestionRepositoryTests.createSampleData(questionRepository);
+
+		Question q = questionRepository.findById(1).get();
+
+		Answer a1 = new Answer();
+		a1.setContent("sbb는 질문답변 게시판입니다.");
+		a1.setQuestion(q);
+		a1.setCreateDate(LocalDateTime.now());
+		answerRepository.save(a1);
+
+		Answer a2 = new Answer();
+		a2.setContent("sbb에서는 주로 스프링 관련 내용을 다룹니다.");
+		a2.setQuestion(q);
+		a2.setCreateDate(LocalDateTime.now());
+		answerRepository.save(a2);
 	}
 
 	private void clearData() {
@@ -40,5 +56,11 @@ class AnswerRepositoryTests {
 		a.setQuestion(q);
 		a.setCreateDate(LocalDateTime.now());
 		answerRepository.save(a);
+	}
+
+	@Test
+	void 조회() {
+		Answer a = answerRepository.findById(1).get();
+		assertThat(a.getContent()).isEqualTo("sbb는 질문답변 게시판입니다.");
 	}
 }
