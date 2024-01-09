2c358f419f7d44e1096dcad361bdae22657a1f24
isjmovo <isjmovo@gmail.com>
2024-01-09 11:32:14 +0900

    findBySubjectLike

--- src/test/java/com/ll/exam/sbb/SbbApplicationTests.java
+++ src/test/java/com/ll/exam/sbb/SbbApplicationTests.java
@@ -65,4 +65,11 @@ class SbbApplicationTests {
 		);
 		assertEquals(1, q.getId());
 	}
+
+	@Test
+	void testJpa5() {
+		List<Question> qList = questionRepository.findBySubjectLike("sbb%");
+		Question q = qList.get(0);
+		assertEquals("sbb가 무엇인가요?", q.getSubject());
+	}
 }
