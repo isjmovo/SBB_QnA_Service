65d42288965a0acaa82c6ee08fb620c7ea59f95d
isjmovo <isjmovo@gmail.com>
2024-01-10 11:35:08 +0900

    8080으로 접속 시 /question/list 로 리다이렉트

--- src/main/java/com/ll/exam/sbb/MainController.java
+++ src/main/java/com/ll/exam/sbb/MainController.java
@@ -6,9 +6,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
-  @GetMapping("/hello")
-  @ResponseBody
-  public String hello() {
-    return "Hello World";
+  @GetMapping("/")
+  public String root() {
+    return "redirect:/question/list";
   }
 }
