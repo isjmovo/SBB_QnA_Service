99c6d42893c4213a92ed3a086a5814f71dcc036c
isjmovo <isjmovo@gmail.com>
2024-01-10 11:07:35 +0900

    question_list.html 뷰 파일 추가

--- src/main/java/com/ll/exam/sbb/Question/QuesitonController.java
+++ src/main/java/com/ll/exam/sbb/Question/QuesitonController.java
@@ -2,12 +2,10 @@ package com.ll.exam.sbb.Question;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class QuesitonController {
   @GetMapping("/question/list")
-  @ResponseBody
   public String list() {
     return "question_list";
   }
