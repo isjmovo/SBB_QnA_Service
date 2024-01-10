c8ad5284c513bee4c486f5cf19cbb8b4a4c5e5ab
isjmovo <isjmovo@gmail.com>
2024-01-10 10:46:31 +0900

    answer를 저장하는 올바른 방법으로 변경

--- src/test/java/com/ll/exam/sbb/AnswerRepositoryTests.java
+++ src/test/java/com/ll/exam/sbb/AnswerRepositoryTests.java
@@ -35,19 +35,15 @@ class AnswerRepositoryTests {
 
 		Answer a1 = new Answer();
 		a1.setContent("sbb는 질문답변 게시판입니다.");
-		a1.setQuestion(q);
 		a1.setCreateDate(LocalDateTime.now());
+		q.addAnswer(a1);
 		answerRepository.save(a1);
-
-		q.getAnswerList().add(a1);
 
 		Answer a2 = new Answer();
 		a2.setContent("sbb에서는 주로 스프링 관련 내용을 다룹니다.");
-		a2.setQuestion(q);
 		a2.setCreateDate(LocalDateTime.now());
+		q.addAnswer(a1);
 		answerRepository.save(a2);
-
-		q.getAnswerList().add(a2);
 
 		questionRepository.save(q);
 	}
@@ -60,6 +56,8 @@ class AnswerRepositoryTests {
 	}
 
 	@Test
+	@Transactional
+	@Rollback(false)
 	void 저장() {
 		Question q = questionRepository.findById(2).get();
 
@@ -71,12 +69,16 @@ class AnswerRepositoryTests {
 	}
 
 	@Test
+	@Transactional
+	@Rollback(false)
 	void 조회() {
 		Answer a = answerRepository.findById(1).get();
 		assertThat(a.getContent()).isEqualTo("sbb는 질문답변 게시판입니다.");
 	}
 
 	@Test
+	@Transactional
+	@Rollback(false)
 	void 관련된_question_조회() {
 		Answer a = answerRepository.findById(1).get();
 		Question q = a.getQuestion();
