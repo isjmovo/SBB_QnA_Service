d8b07d71e6763d51eea70036828cc863510a0c50
isjmovo <isjmovo@gmail.com>
2024-01-10 11:12:51 +0900

    타임리프 캐시 끄기, 안 되는 경우 Ctrl + F9

--- src/main/java/com/ll/exam/sbb/Question/QuesitonController.java
+++ src/main/java/com/ll/exam/sbb/Question/QuesitonController.java
@@ -1,12 +1,23 @@
 package com.ll.exam.sbb.Question;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor  // 생성자 주입
 public class QuesitonController {
+  private final QuestionRepository questionRepository;
+
   @GetMapping("/question/list")
-  public String list() {
+  public String list(Model model) {
+    List<Question> questionList = questionRepository.findAll();
+
+    model.addAttribute("questionList", questionList);
+
     return "question_list";
   }
 }
