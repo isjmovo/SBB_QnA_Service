9e1422ff5d99d654eb5580453291e81c76cef92b
isjmovo <isjmovo@gmail.com>
2024-01-15 10:29:52 +0900

    질문 폼 체크로직

---
+++ src/main/java/com/ll/exam/sbb/Question/QuestionForm.java
@@ -0,0 +1,11 @@
+package com.ll.exam.sbb.Question;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class QuestionForm {
+  public String subject;
+  public String content;
+}
